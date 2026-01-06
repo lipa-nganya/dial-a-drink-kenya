cd8faef9ab6897292d4f8df10f2ed0b467161110
lipa-nganya <lipanganya@gmail.com>
2026-01-06 14:58:33 +0300

    Fix: use custom event emitter compatible with React Native

--- DDDriverExpo/src/utils/notificationEvents.js
+++ DDDriverExpo/src/utils/notificationEvents.js
@@ -1,6 +1,35 @@
-import { EventEmitter } from 'events';
-
-// Global event emitter for notification events
+// Simple event emitter for notification events
 // This allows the notification handler to trigger the overlay
-export const notificationEvents = new EventEmitter();
+class NotificationEventEmitter {
+  constructor() {
+    this.listeners = {};
+  }
+
+  on(event, callback) {
+    if (!this.listeners[event]) {
+      this.listeners[event] = [];
+    }
+    this.listeners[event].push(callback);
+  }
+
+  off(event, callback) {
+    if (this.listeners[event]) {
+      this.listeners[event] = this.listeners[event].filter(cb => cb !== callback);
+    }
+  }
+
+  emit(event, data) {
+    if (this.listeners[event]) {
+      this.listeners[event].forEach(callback => {
+        try {
+          callback(data);
+        } catch (error) {
+          console.error('Error in notification event listener:', error);
+        }
+      });
+    }
+  }
+}
+
+export const notificationEvents = new NotificationEventEmitter();
 
